62f495aae06bcda8be09d4fba31b28f8195fee59
AdrianaRoza <adriana.roza.aka@gmail.com>
2022-07-20 12:00:07 -0400

    add-header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from './Imgs/logo.jpg'
+
+export default function Header(){
+    return(
+        <header>
+            <h1>Designer</h1>
+            <img src={Logo}/>
+        </header>
+        
+    )
+}
